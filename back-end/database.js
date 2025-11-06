7ed681288be07bbd08af7917d369af0abeefa3a5
estelacamila <estelacamila522@icloud.com>
2025-11-06 09:28:17 -0300

    teste1

--- back-end/database.js
+++
@@ -1,6 +0,0 @@
-import postgres from "postgres";
-
-const sql = postgres('postgres://root:root@192.168.1.15:5432/Duda');
-
-
-export default sql;
---
+++ backEnd/database.js
@@ -0,0 +1,6 @@
+import postgres from "postgres";
+
+const sql = postgres('postgresql://postgres.njbbjqrfzdcpkfskdyfi:@Ij17172020@aws-1-us-east-2.pooler.supabase.com:5432/postgres');
+
+
+export default sql;
